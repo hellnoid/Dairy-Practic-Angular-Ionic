c7870fb4ad1678f407f4fb304c1e505bef2c0447
hellnoid <xainhellnoid22@gmail.com>
2022-10-19 14:15:36 +0530

    2

--- ionic1/src/app/home/home.page.ts
+++ ionic1/src/app/home/home.page.ts
@@ -5,8 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  reason:string="";
+  amount:number=0;
+  listArray:object[]=[];
+  // listOb:{}={};
+  total:number=0;
 
   constructor() {}
+  clear(){
+    this.amount=0;
+    this.reason="";
+  }
 
+  add(){
+    if(this.reason.trim().length==0||this.amount<=0) return;
+
+    let dumOb:{}={
+    "amount":this.amount,
+    "reason":this.reason
+    }
+
+    this.listArray.push(dumOb);
+
+    // console.log(this.listArray);
+    // console.log("amount  ",this.amount);
+    this.total=this.total+this.amount;
+    // console.log("total   ",this.total);
+
+    this.clear();
+  }
 }
